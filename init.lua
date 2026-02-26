99145ffa0be4b512cbad33ae06b747062bc88bca
soukkis <bun@cozyDen>
2026-02-26 21:01:06 +0200

    render markdown added plus custom heading icons

--- init.lua
+++ init.lua
@@ -1040,7 +1040,23 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+          heading = {
+      -- position = "inline",
+      sign = false,
+      icons = { '󰅂 ', '󰅂󰅂 ', '󰅂󰅂󰅂 ', '󰄾󰄾 ', '󱙧 ', '󰋙 ' },
+    },
+  },
+  },
 
+  
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
